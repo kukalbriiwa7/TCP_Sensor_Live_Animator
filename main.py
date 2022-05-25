dda2d50c2d099b772e268f1e8ee7f40691c6c80a
kukalbriiwa7 <amadani20@ku.edu.tr>
2022-05-26 02:29:41 +0300

    Diagonal and Circular Testing Paths Are Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+from textwrap import fill
 import time
 import timeit
 import matplotlib
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import threading
 import numpy as np
+import math
 
 # Switch matplotlib backend to Qt5Agg
 matplotlib.use('Qt5Agg')
@@ -15,7 +17,7 @@ matplotlib.use('Qt5Agg')
 
 HOST = "192.168.1.15" 
 PORT = 8001
-FREQUENCY = 0.25
+FREQUENCY = 0.15
 
 robot_position_x = 0
 robot_position_z = 0
@@ -99,8 +101,13 @@ class TCPServer(socket.socket):
 
 
 class Drawer():
-    def __init__(self, guide_direction):
-        self.dir = guide_direction                    
+
+    def __init__(self, guide_direction, diameter_1=False , diameter_2=False , circular_cw=False, circular_ccw=False):
+        self.dir = guide_direction
+        self.dia_1 = diameter_1
+        self.dia_2 = diameter_2
+        self.cir_cw = circular_cw
+        self.cir_ccw = circular_ccw                  
         pass
 
     def start_animation(self):
@@ -115,13 +122,30 @@ class Drawer():
 
     def update_plot(self, frame):
 
-        # t = time.time() - self.start_time
+        t = time.time() - self.start_time
         # self.periodic_box.set_width(self.bounding_box.get_width() / 2 * np.sin(2 * np.pi * t * FREQUENCY))
-        if self.dir == 'horizontal':
+        if self.dia_1 == True:
+            self.guide_circle.set_center((25 + guide_position, 22.5 + guide_position))
+            self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z)) 
+
+        elif self.dia_2 == True:
+            self.guide_circle.set_center((25 + guide_position, 22.5 - guide_position))
+            self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z))
+
+        elif self.cir_cw == True:
+            self.guide_circle.set_center((25 + 15*np.cos(2 * np.pi * t * FREQUENCY), 22.5 + 15*np.sin(2 * np.pi * t * FREQUENCY)))
+            self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z))     
+
+        elif self.cir_ccw == True:
+            self.guide_circle.set_center((25 + 15*np.cos(-2 * np.pi * t * FREQUENCY), 22.5 + 15*np.sin(-2 * np.pi * t * FREQUENCY)))
+            self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z)) 
+
+        elif self.dir == 'horizontal':
             self.periodic_box.set_width(guide_position)
             self.guide_line.set_xdata([25 + guide_position, 25 + guide_position])
             self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z)) 
-        else:
+
+        elif self.dir == 'vertical':
             self.periodic_box.set_height(guide_position)
             self.guide_line.set_ydata([22.5 + guide_position, 22.5 + guide_position])
             self.cursor_circle.set_center((25 + robot_position_x, 22.5 + robot_position_z))            
@@ -129,7 +153,7 @@ class Drawer():
     def initilize_plot(self):
         self.start_time = time.time()
         self.ax.set_aspect('equal')
-        self.ax.set_title('Live Robot Position')
+        self.ax.set_title('Live Robot Position', fontsize=20, fontweight='bold')
         self.ax.grid(False, which='both') 
         self.ax.spines['top'].set_visible(False)
         self.ax.spines['right'].set_visible(False)
@@ -139,32 +163,94 @@ class Drawer():
         self.ax.set_ylim(0, 50)
         self.ax.tick_params(
             axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
-        if self.dir == 'horizontal':    
-            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k')
+
+        if self.dia_1 == True:
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
             horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
             self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
-                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k')
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
+            self.cursor_circle = plt.Circle((25,22.5), 2, color='b')
+            self.ax.add_line(self.horizontal_line)
+            self.ax.add_line(self.vertical_line)
+            self.ax.add_patch(self.cursor_circle)
+            self.diagonal_line_1 = plt.Line2D([25-20,25+20],[22.5-20,22.5+20], color='k', linestyle='--')
+            self.ax.add_line(self.diagonal_line_1)
+            self.guide_circle = plt.Circle((25,22.5), 3, color='k', fill=False)
+            self.ax.add_patch(self.guide_circle)
+
+        elif self.dia_2 == True:
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
+            horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
+            self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
+            self.cursor_circle = plt.Circle((25,22.5), 2, color='b')
+            self.ax.add_line(self.horizontal_line)
+            self.ax.add_line(self.vertical_line)
+            self.ax.add_patch(self.cursor_circle)
+            self.diagonal_line_2 = plt.Line2D([25-20,25+20],[22.5+20,22.5-20], color='k', linestyle='--')
+            self.ax.add_line(self.diagonal_line_2)
+            self.guide_circle = plt.Circle((25,22.5), 3, color='k', fill=False)
+            self.ax.add_patch(self.guide_circle)
+
+        elif self.cir_cw == True:
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
+            horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
+            self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
+            self.cursor_circle = plt.Circle((25,22.5), 2, color='b')
+            self.ax.add_line(self.horizontal_line)
+            self.ax.add_line(self.vertical_line)
+            self.ax.add_patch(self.cursor_circle)
+            self.path_circle = plt.Circle((25,22.5), 15, color='k', fill=False, linestyle='--')
+            self.ax.add_patch(self.path_circle)
+            self.guide_circle = plt.Circle((25+15,22.5), 3, color='k', fill=False)
+            self.ax.add_patch(self.guide_circle)
+
+        elif self.cir_ccw == True:
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
+            horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
+            self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
+            self.cursor_circle = plt.Circle((25,22.5), 2, color='b')
+            self.ax.add_line(self.horizontal_line)
+            self.ax.add_line(self.vertical_line)
+            self.ax.add_patch(self.cursor_circle)
+            self.path_circle = plt.Circle((25,22.5), 15, color='k', fill=False, linestyle='--')
+            self.ax.add_patch(self.path_circle)
+            self.guide_circle = plt.Circle((25+15,22.5), 3, color='k', fill=False)
+            self.ax.add_patch(self.guide_circle)
+
+        elif self.dir == 'horizontal':    
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
+            horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
+            self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
             self.periodic_box = plt.Rectangle((25, 21.25), 0, 2.5, fc='r')
             self.guide_line = plt.Line2D([25,25], [19,26], color='r', lw=2)
             self.cursor_circle = plt.Circle((25,22.5), 2, color='b')
+            # self.guide_circle = plt.Circle((25,22.5), 15, color='k', fill=False, linestyle='--')
             self.ax.add_patch(self.periodic_box)
             self.ax.add_line(self.horizontal_line)
             self.ax.add_line(self.vertical_line)
             self.ax.add_patch(self.cursor_circle)
             self.ax.add_line(self.guide_line)
-        else:
-            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k')
+            # self.ax.add_patch(self.guide_circle)
+
+        elif self.dir == 'vertical':
+            self.horizontal_line = plt.Line2D([5,45],[22.5,22.5], color='k', linewidth=3)
             horizonal_line_length = self.horizontal_line.get_xdata()[1] - self.horizontal_line.get_xdata()[0]
             self.vertical_line = plt.Line2D([25,25],[self.horizontal_line.get_ydata(orig = True)[0]+horizonal_line_length/2,
-                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k')
+                                            self.horizontal_line.get_ydata(orig = True)[0]-horizonal_line_length/2], color='k', linewidth=3)
             self.periodic_box = plt.Rectangle((23.75, 22.5), 2.5, 0, fc='r')
             self.guide_line = plt.Line2D([21.5,28.5], [22.5,22.5], color='r', lw=2)
             self.cursor_circle = plt.Circle((22.5,25), 2, color='b')
+            # self.guide_circle = plt.Circle((25,22.5), 15, color='k', fill=False, linestyle='--')
             self.ax.add_patch(self.periodic_box)
             self.ax.add_line(self.horizontal_line)
             self.ax.add_line(self.vertical_line)
             self.ax.add_patch(self.cursor_circle)
-            self.ax.add_line(self.guide_line)   
+            self.ax.add_line(self.guide_line)
+            # self.ax.add_patch(self.guide_circle)
 
     def close(self):
         plt.close()
